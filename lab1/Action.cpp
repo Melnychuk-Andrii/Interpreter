1d0c15879012bb1759be7554b393b7d1a2b00b16
Andrew Melnychuk <androkep@gmail.com>
2020-03-26 17:47:51 +0200

    version 1.03 minor fixes

--- lab1/Action.cpp
+++
@@ -1,9 +0,0 @@
-#include "Action.h"
-#include "Token_type_strings.h"
-
-std::string Action::represent()
-{
-	std::string res = "";
-	res = res + keywords[act] + "(" + std::to_string(val)+")\r\n";
-    return res;
-}
